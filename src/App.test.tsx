6778b3758c255fd1dc5bb15442e7df5bd00d4095
TAYLOR KADANS <tkadans@udel.edu>
2023-11-12 21:08:30 -0500

    sent

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -134,4 +134,178 @@ describe("App Tests", () => {
         deleteYear.click();
         expect(screen.queryByLabelText("Freshman")).not.toBeInTheDocument();
     });
+    test("Users can add a new semester to a year", () => {
+        // Opens Degree Plan and Adds a Sample Degree
+        const degreeButton = screen.getByText("Degrees");
+        degreeButton.click();
+        const button = screen.getByText("Add");
+        button.click();
+        const saveButtonDegree = screen.getByText("Save");
+        saveButtonDegree.click();
+
+        // Opens Sample Degree
+        const text = screen.getByText("Sample Degree");
+        text.click();
+
+        // Adds a Freshman Year
+        const addYear = screen.getByText("Add Year");
+        addYear.click();
+        const saveButtonYear = screen.getByText("Save");
+        saveButtonYear.click();
+
+        // Adds a new semester
+        const addSemester = screen.getByText("Add Semester");
+        addSemester.click();
+        const saveSemester = screen.getByText("Save");
+        saveSemester.click();
+
+        expect(screen.queryByText("Edit Semester")).toBeInTheDocument();
+        expect(screen.queryByText("Delete Semester")).toBeInTheDocument();
+        expect(screen.queryByText("Fall Semester 2023")).toBeInTheDocument();
+    });
+
+    test("Users can delete a semester", () => {
+        // Opens Degree Plan and Adds a Sample Degree
+        const degreeButton = screen.getByText("Degrees");
+        degreeButton.click();
+        const button = screen.getByText("Add");
+        button.click();
+        const saveButtonDegree = screen.getByText("Save");
+        saveButtonDegree.click();
+
+        // Opens Sample Degree
+        const text = screen.getByText("Sample Degree");
+        text.click();
+
+        // Adds a Freshman Year
+        const addYear = screen.getByText("Add Year");
+        addYear.click();
+        const saveButtonYear = screen.getByText("Save");
+        saveButtonYear.click();
+
+        // Adds a new semester
+        const addSemester = screen.getByText("Add Semester");
+        addSemester.click();
+        const saveSemester = screen.getByText("Save");
+        saveSemester.click();
+
+        expect(screen.queryByText("Delete Semester")).toBeInTheDocument();
+        const deleteSemester = screen.getByText("Delete Semester");
+        expect(screen.queryByText("Fall Semester 2023")).toBeInTheDocument();
+        deleteSemester.click();
+        expect(
+            screen.queryByLabelText("Fall Semester 2023")
+        ).not.toBeInTheDocument();
+    });
+
+    test("Users can edit a semester", () => {
+        // Opens Degree Plan and Adds a Sample Degree
+        const degreeButton = screen.getByText("Degrees");
+        degreeButton.click();
+        const button = screen.getByText("Add");
+        button.click();
+        const saveButtonDegree = screen.getByText("Save");
+        saveButtonDegree.click();
+
+        // Opens Sample Degree
+        const text = screen.getByText("Sample Degree");
+        text.click();
+
+        // Adds a Freshman Year
+        const addYear = screen.getByText("Add Year");
+        addYear.click();
+        const saveButtonYear = screen.getByText("Save");
+        saveButtonYear.click();
+
+        // Adds a new semester
+        const addSemester = screen.getByText("Add Semester");
+        addSemester.click();
+        const saveSemester = screen.getByText("Save");
+        saveSemester.click();
+
+        expect(screen.queryByText("Edit Semester")).toBeInTheDocument();
+        const editSemester = screen.getByText("Edit Semester");
+        expect(screen.queryByText("Fall Semester 2023")).toBeInTheDocument();
+        editSemester.click();
+        expect(
+            screen.queryByText("Fall Semester 2023 ID: 1")
+        ).toBeInTheDocument();
+    });
+
+    test("Users can add a course to a semester", () => {
+        // Opens Degree Plan and Adds a Sample Degree
+        const degreeButton = screen.getByText("Degrees");
+        degreeButton.click();
+        const button = screen.getByText("Add");
+        button.click();
+        const saveButtonDegree = screen.getByText("Save");
+        saveButtonDegree.click();
+
+        // Opens Sample Degree
+        const text = screen.getByText("Sample Degree");
+        text.click();
+
+        // Adds a Freshman Year
+        const addYear = screen.getByText("Add Year");
+        addYear.click();
+        const saveButtonYear = screen.getByText("Save");
+        saveButtonYear.click();
+
+        // Adds a new semester
+        const addSemester = screen.getByText("Add Semester");
+        addSemester.click();
+        const saveSemester = screen.getByText("Save");
+        saveSemester.click();
+
+        // Add courses
+        const editSemester = screen.getByText("Edit Semester");
+        editSemester.click();
+        const showCourses = screen.getByText("Show Courses");
+        showCourses.click();
+        const addCourse = screen.getAllByText("Add")[1];
+        addCourse.click();
+        const save = screen.getByText("Save");
+        save.click();
+        expect(screen.queryByText("Remove")).toBeInTheDocument();
+    });
+
+    test("Users can remove a course from a semester", () => {
+        // Opens Degree Plan and Adds a Sample Degree
+        const degreeButton = screen.getByText("Degrees");
+        degreeButton.click();
+        const button = screen.getByText("Add");
+        button.click();
+        const saveButtonDegree = screen.getByText("Save");
+        saveButtonDegree.click();
+
+        // Opens Sample Degree
+        const text = screen.getByText("Sample Degree");
+        text.click();
+
+        // Adds a Freshman Year
+        const addYear = screen.getByText("Add Year");
+        addYear.click();
+        const saveButtonYear = screen.getByText("Save");
+        saveButtonYear.click();
+
+        // Adds a new semester
+        const addSemester = screen.getByText("Add Semester");
+        addSemester.click();
+        const saveSemester = screen.getByText("Save");
+        saveSemester.click();
+
+        // Add courses
+        const editSemester = screen.getByText("Edit Semester");
+        editSemester.click();
+        const showCourses = screen.getByText("Show Courses");
+        showCourses.click();
+        const addCourse = screen.getAllByText("Add")[1];
+        addCourse.click();
+        const save = screen.getByText("Save");
+        save.click();
+        expect(screen.queryByText("Remove")).toBeInTheDocument();
+        const remove = screen.getByText("Remove");
+        remove.click();
+        expect(screen.queryByText("Remove")).not.toBeInTheDocument();
+    });
 });
