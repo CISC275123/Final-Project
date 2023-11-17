f927826ae47d9f824c4c92b4f04a68db5d2d7a69
TAYLOR KADANS <tkadans@udel.edu>
2023-11-17 15:01:31 -0500

    fixed credits

--- src/components/SemesterView.tsx
+++ src/components/SemesterView.tsx
@@ -27,7 +27,12 @@ export const SemesterView = ({
     function saveInfo() {
         semester.notes = semester.notes + description;
         for (const x of addedCourses) {
-            const y = +x.credits;
+            let y = 0;
+            if (x.credits.includes("-")) {
+                y = +x.credits.substring(x.credits.length - 1);
+            } else {
+                y = +x.credits;
+            }
             if (
                 ((semester.currentCredits + y) as unknown as number) <=
                 semester.maxCredits
@@ -48,9 +53,15 @@ export const SemesterView = ({
         semester.courses = [];
         semester.currentCredits = 0;
     }
-    function removeCourse(id: string, creds: number) {
+    function removeCourse(id: string, creds: string) {
         semester.courses = semester.courses.filter((c: Course) => c.code != id);
-        semester.currentCredits = semester.currentCredits - creds;
+        let y = 0;
+        if (creds.includes("-")) {
+            y = +creds.substring(creds.length - 1);
+        } else {
+            y = +creds;
+        }
+        semester.currentCredits = semester.currentCredits - y;
         saveInfo();
     }
     return (
@@ -65,11 +76,7 @@ export const SemesterView = ({
             {semester.courses.map((c) => (
                 <div key={c.code}>
                     {c.code}{" "}
-                    <Button
-                        onClick={() =>
-                            removeCourse(c.code, c.credits as unknown as number)
-                        }
-                    >
+                    <Button onClick={() => removeCourse(c.code, c.credits)}>
                         Remove
                     </Button>
                 </div>
